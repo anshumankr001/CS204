91315e0da50149d1986e3d2057a033be7704fdf9
anshumankr001 <53443801+anshumankr001@users.noreply.github.com>
2019-08-26 16:54:06 +0530

    Create ExpEval.cpp

---
+++ Lab_5/ExpEval.cpp
@@ -0,0 +1,214 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define f(i,a,b) for(int i=a; i<b; i++)
+#define pb push_back
+
+bool isOperator(string c)
+{ 
+    if (c == "+" || c == "-" || c == "*" || c == "/" || c == "^") return true;
+    return false;
+}
+
+bool isOperator(char c)
+{ 
+    if (c == '+' || c == '-' || c == '*' || c == '/' || c == '^') return true;
+    return false;
+}
+
+int isfchar(string s)
+{
+    if((s[0] >= '0' && s[0] <= '9') || s[0] == '-' || s[0] == '+') return 0;
+    return 1;
+}
+
+struct tree
+{
+    string str;
+    tree* left;
+    tree* right;
+};
+
+tree* newNode(string s)
+{
+    tree* temp = new tree;
+    temp->left = NULL;
+    temp->right = NULL;
+    temp->str = s;
+    return temp;
+}
+
+bool isInt(string s)
+{
+    int n = s.length();
+    for (int i = 0; i < n; i++)
+    {
+        if(i == 0 && s[i] == '-') i++;
+        if (isdigit(s[i]) == false) return false;
+    }
+    return true;
+}
+
+int prec(string c)
+{
+    if(c == "^") return 5;
+    else if(c == "/") return 4;
+    else if(c == "*") return 3;
+    else if(c == "+") return 2;
+    else if(c == "-") return 1;
+    else return -1;
+}
+
+vector<string> inToPost(string str)
+{
+    stack<string> st;
+    st.push("N");
+    vector<string> ans, s;
+    string t;
+    int x = str.size();
+    f(i, 0, x)
+    {
+        if(str[i] == '+' && i == 0) continue;
+        if( (str[i] >= '0' && str[i] <= '9') || ((isOperator(str[i-1]) || str[i-1] == '(') && str[i] == '-') || (i == 0 && str[i] == '-') ) t += str[i];
+        else
+        {
+            if(!t.empty()) s.pb(t);
+            t.clear();
+            t += str[i];
+            if(!t.empty()) s.pb(t);
+            t.clear();
+        }
+        if(i == x-1) if(!t.empty()) s.pb(t);
+    }
+    int l = s.size();
+    for(int i=0; i<l; i++)
+    {
+        if(isInt(s[i])) ans.pb(s[i]);
+        else if(s[i] == "(") st.push("(");
+        else if(s[i] == ")")
+        {
+            while(st.top() != "N" && st.top() != "(")
+            {
+                string c = st.top();
+                st.pop();
+                ans.pb(c);
+            }
+            if(st.top() == "(") st.pop();
+        }
+        else
+        {
+            while(st.top() != "N" && prec(s[i]) <= prec(st.top()))
+            {
+                string c = st.top();
+                st.pop();
+                ans.pb(c);
+            }
+            st.push(s[i]);
+        }
+    }
+    while(st.top() != "N")
+    {
+        string c = st.top();
+        st.pop();
+        ans.pb(c);
+    }
+    return ans;
+}
+
+tree* makeTree(string s)
+{
+    vector<string> vs = inToPost(s);
+    stack<tree *> st;
+    tree *t, *t1, *t2;
+    int n = vs.size();
+    for (int i=0; i<n; i++)
+    {
+        if (isInt(vs[i]))
+        {
+            t = newNode(vs[i]);
+            st.push(t);
+        }
+        else
+        {
+            t = newNode(vs[i]);
+            t1 = st.top();
+            st.pop();
+            t2 = st.top();
+            st.pop();
+            t->right = t1;
+            t->left = t2;
+            st.push(t);
+        }
+    }
+    t = st.top();
+    st.pop();
+    return t;
+}
+
+int eval(tree* a)
+{
+	if(a->left == NULL && a->right == NULL) return stoi(a->str);
+	int A = eval(a->left);
+	int B = eval(a->right);
+	if(a->str == "+") return A+B;
+	else if(a->str == "-") return A-B;
+	else if(a->str == "*") return A*B;
+	else if(a->str == "%") return A%B;
+	else if(a->str == "/") return A/B;
+	else if(a->str == "^") return pow(A,B);
+    else return 0;
+}
+
+string unary(string s)
+{
+    string t, u;
+    int x = s.size(), j=0;
+    int p[x] = {};
+    for(int i=0; i<x; i++)
+    {
+        if(s[i] == '-' && s[i+1] == '-') p[j]++;
+        else if(p[j] != 0 && p[j]%2 == 0)
+        {
+            t += '-';
+            j++;
+        }
+        else if(p[j] != 0 && p[j]%2 == 1)
+        {
+            t += '+';
+            j++;
+        }
+           else t += s[i];
+    }
+       int y = t.size();
+    for(int i=0; i<y; i++)
+    {
+        if(t[i] == '+' && t[i+1] == '+') continue;
+        if(t[i] == '-' && t[i+1] == '(') u += "-1*";
+        else u += t[i];
+    }
+    return u;
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        while(n--)
+        {
+            string s, t, u;
+            cin>>s;
+            s = unary(s);
+            vector <string> vs;
+            vs = inToPost(s);
+            if(!isfchar(t))
+            {
+                tree* f = makeTree(u);
+                cout<<endl<<eval(f);
+            }
+        }
+    }
+    return 0;
+}
