69aaa87f27262f1ffdc39b18c1682383cf70e6fe
anshumankr001 <53443801+anshumankr001@users.noreply.github.com>
2019-09-25 09:45:50 +0530

    Update BlumSelection.cpp

--- PostMidsemLab_1/BlumSelection.cpp
+++ PostMidsemLab_1/BlumSelection.cpp
@@ -9,10 +9,13 @@ void swapll(ll *a, ll *b)
     *b = temp;
 }
 
-ll partition(ll arr[], ll l, ll r)
+ll partition(ll arr[], ll l, ll r, ll x)
 {
-    ll x = arr[r], i = l;
-    for (ll j=l; j <= r-1; j++)
+    ll i;
+    for (i=l; i<r; i++) if (arr[i] == x) break; 
+    swapll(&arr[i], &arr[r]);
+    i = l;
+    for (ll j = l; j <= r - 1; j++)
     {
         if (arr[j] <= x)
         {
@@ -24,12 +27,27 @@ ll partition(ll arr[], ll l, ll r)
     return i;
 }
 
+ll findMedian(ll arr[], ll n)
+{
+    sort(arr, arr+n);
+    return arr[n/2];
+}
+
 ll kthSmallest(ll arr[], ll l, ll r, ll k)
 {
-    if (k > 0 && k <= r-l+1)
+    if (k > 0 && k <= r - l + 1)
     {
-        ll pos = partition(arr, l, r);
-        if (pos-l == k-1) return arr[pos]; 
+        ll n = r-l+1;
+        ll i, median[(n+4)/5];
+        for (i=0; i<n/5; i++) median[i] = findMedian(arr+l+i*5, 5);
+        if (i*5 < n)
+        {
+            median[i] = findMedian(arr+l+i*5, n%5);
+            i++;
+        }
+        ll medOfMed = (i == 1)? median[i-1]: kthSmallest(median, 0, i-1, i/2);
+        ll pos = partition(arr, l, r, medOfMed);
+        if (pos-l == k-1) return arr[pos];
         if (pos-l > k-1) return kthSmallest(arr, l, pos-1, k);
         return kthSmallest(arr, pos+1, r, k-pos+l-1);
     }
@@ -53,7 +71,7 @@ int main()
         if(n%2 == 0) k = n/2;
         else k = n/2 + 1;
         double sqrtdis = sqrt((double)kthSmallest(dis, 0, n-1, k));
-        cout<<sqrtdis<<endl;
+        printf("%.15lf\n", sqrtdis);
     }
     return 0;
 }
